3a9b739b58502f699bb9818b7188aa6ed97a74cf
yuuki <yuki.tsubo@gmail.com>
2023-02-16 20:18:34 +0900

    use ResponseEntityExceptionHandler for error handling

--- src/main/java/works/weave/socks/cart/controllers/ErrorController.java
+++ src/main/java/works/weave/socks/cart/controllers/ErrorController.java
@@ -1,16 +1,17 @@
 package works.weave.socks.cart.controllers;
 
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ErrorController {
+public class ErrorController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgumentException(IllegalAccessException ex, HttpServletResponse response) {
-        response.setStatus(400);
-        return ex.getMessage();
-    }
+	public ResponseEntity<Object> handleIllegalArgumentException(IllegalAccessException ex, WebRequest request) {
+		return super.handleExceptionInternal(ex, "handleIllegalArgumentException", null, HttpStatus.BAD_REQUEST, request);
+	}
 }
